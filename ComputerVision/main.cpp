bba97ca03543a59517d41af86dd5ed65f96cf77b
TobiaPeruzzi <68054589+TobiaPeruzzi@users.noreply.github.com>
2023-04-11 14:47:19 +0200

    Update main.cpp

--- ComputerVision/main.cpp
+++ ComputerVision/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-  auto gradient = cv::algorithms::SobelGradient("C:\\Users\\t.peruzzi\\Desktop\\Valve_original_(1).png", 20, true);
+  auto gradient = cv::algorithms::SobelGradient("", 20, true);
   gradient.Apply();
-  gradient.Save("C:\\Users\\t.peruzzi\\Desktop\\testConv2.png");
+  gradient.Save("");
 }
