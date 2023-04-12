38ee69c39b35cba6780b1d7bc6760fe6c1e698c1
Tobia Peruzzi <t.peruzzi@outlook.com>
2023-04-12 08:26:38 +0200

    minor changes

--- ComputerVision/main.cpp
+++ ComputerVision/main.cpp
@@ -4,7 +4,7 @@
 
 int main()
 {
-  auto gradient = cv::algorithms::SobelGradient("C:\\Users\\t.peruzzi\\Desktop\\phone_img.png", 100, true);
+  auto gradient = cv::algorithms::SobelGradient("", 100, true);
   gradient.Apply();
-  gradient.Save("C:\\Users\\t.peruzzi\\Desktop\\conv_phone_img_2.png");
+  gradient.Save("");
 }
